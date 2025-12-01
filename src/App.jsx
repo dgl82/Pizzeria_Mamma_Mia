062f12bbd6d79178a4eab78e779815d05beeb0c6
David Gili <d.gili.lopez@gmail.com>
2025-12-01 19:12:34 -0300

    footer agregado y overflow solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Home></Home>
+      <Footer></Footer>
     </>
   );
 }
