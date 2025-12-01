5d8c089686f6cc2464dc37dafcae72960abb68a9
David Gili <d.gili.lopez@gmail.com>
2025-12-01 16:57:21 -0300

    header terminado

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="fondo">
+      <h3>¡Pizzería Mamma Mía!</h3>
+      <h4>¡Tenemos las mejores pizzas que podrás encontrar!</h4>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import Header from "./Header";
+
+const Home = () => {
+  return <Header></Header>;
+};
+
+export default Home;
